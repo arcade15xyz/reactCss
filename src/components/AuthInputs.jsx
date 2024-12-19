4ca0ff16410a2309605dd689add2adcab35c9ea2
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-19 12:43:46 +0530

    dynamic tailwind css done

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -30,27 +30,25 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
             label="Email"
             invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
           <Input
             label="Password"
             invalid={emailNotValid}
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-100 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
